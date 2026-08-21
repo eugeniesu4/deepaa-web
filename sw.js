18358e6439d78ddb6dd082449383410e3308bb00
eugeniesu4 <47984021+eugeniesu4@users.noreply.github.com>
2026-08-22 02:32:14 +0800

    Disable broken service worker asset proxy

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const UPSTREAM='https://raw.githubusercontent.com/n317/DeepAAonWeb/master/docs/';
-const ASSETS=new Set(['app.bundle.js','model/model_v2.bin','model/model.json','model/model_metadata.json','model/model_weights.buf','fonts/Saitamaar.woff2','fonts/Saitamaar.ttf','fonts/Saitamaar.eot','sample-data/test_image.png']);
-self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>{
-  const url=new URL(event.request.url);
-  const path=url.pathname.replace(/^\/(?:deepaa-web\/)?/,'');
-  if(!ASSETS.has(path)) return;
-  event.respondWith(fetch(UPSTREAM+path,{mode:'cors',cache:'no-cache'}));
+// DeepAA Web 2.3: disable the old service worker proxy.
+// The previous version redirected model/assets to raw.githubusercontent.com,
+// which could leave Chrome with stale/broken resources.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.registration.unregister();
+  })());
 });
